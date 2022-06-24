c353254ee9dd8a01a66b1363477289f4b2c540e5
huangfeng <huangfeng0127@hotmail.com>
2022-06-25 07:54:27 +0800

    first commit

---
+++ Day1.py
@@ -0,0 +1,4 @@
+print("Welcome to the band name creator!")
+city = input("please input your city name:\n")
+pet = input("please input a pet name:\n")
+print("your band name could be: " + city + " " + pet)
---
+++ Day2.py
@@ -0,0 +1,5 @@
+# body mass index  BMI = weight(kg)/height*height(m)
+height = input("enter your height in m: \n")
+weight = input("enter your weight in kg: \n")
+bmi = float(weight)/float(height)**2
+print(bmi)
