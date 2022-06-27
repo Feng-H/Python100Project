3c2dc4c08bacdbb1efd6a0ecae9c92c2f23d4b52
huangfeng <huangfeng0127@hotmail.com>
2022-06-27 19:19:12 +0800

    Day3 update verse 3

--- Day3.py
+++ Day3.py
@@ -1,3 +1,5 @@
+## Bill split
+
 # height = input("Please input your head in cm:")
 # height_as_int = int(height)
 # if height_as_int >= 120:
@@ -11,20 +13,47 @@
 #         print("you should pay 12$.")
 # else :
 #     print("You can not use Roadrider!")
-from pyrsistent import b
 
+## bmi 2.0
+# height = float(input("Please input your head in m:"))
+# weight = float(input("please input your weight in kg:"))
+# bmi = round(weight/height**2)
+# if bmi < 18.5:
+#     print(f"Your bim is {bmi}, you are underweight.")
+# elif bmi < 25:
+#     print(f"Your bmi is {bmi}, you are a normal weight.")
+# elif bmi < 30:
+#     print(f"Your bmi is {bmi}, you are overweight.")
+# elif bmi < 35:
+#     print(f"Your bmi is {bmi}, you are obese.")
+# else:
+#     print(f"Your bmi is {bmi}, you are clinically obese.")
 
-height = float(input("Please input your head in m:"))
-weight = float(input("please input your weight in kg:"))
+## a year is leap year
+## 可以被4整除，但是不能被100整除
+## 可以被400整除
 
-bmi = round(weight/height**2)
-if bmi < 18.5:
-    print(f"Your bim is {bmi}, you are underweight.")
-elif bmi < 25:
-    print(f"Your bmi is {bmi}, you are a normal weight.")
-elif bmi < 30:
-    print(f"Your bmi is {bmi}, you are overweight.")
-elif bmi < 35:
-    print(f"Your bmi is {bmi}, you are obese.")
+# leap year judgement
+year = int(input("Please input the year: "))
+
+# logic one
+if year%400 == 0 :
+    print(f"this year : {year} is a leap year")
+elif year%100 == 0:
+    print(f"this year : {year} is not a leap year")
+elif year%4 == 0:
+    print(f"this year : {year} is a leap year")
 else:
-    print(f"Your bmi is {bmi}, you are clinically obese.")
+    print(f"this year :{year} is not a leap year")
+
+# logic two
+if year%4 == 0:
+    if year%100 ==0:
+        if year%400 ==0:
+            print(f"this year : {year} is a leap year")
+        else:
+            print(f"this year : {year} is not a leap year")
+    else :
+        print(f"this year : {year} is a leap year")
+else :
+    print(f"this year : {year} is not a leap year")
