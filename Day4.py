4dddb4d46cca1ba57366d575b9bb6c6feded90c2
huangfeng <huangfeng0127@hotmail.com>
2022-06-29 20:25:47 +0800

    day4 update2

--- Day4.py
+++ Day4.py
@@ -1,7 +1,14 @@
 # randomisation
-import random
-random_integer = random.randint(1, 10)
-print(random_integer)
+# import random
+# random_integer = random.randint(1, 10)
+# print(random_integer)
 
-random_float = random.random()
-print(random_float)
+# random_float = random.random()
+# print(random_float)
+
+# list
+import random
+names_string = input("Input the names, seperation with common : ")
+names = names_string.split(", ")
+random_choice =random.randint(0,len(names)-1)
+print(f"{names[random_choice]} will pay today")
