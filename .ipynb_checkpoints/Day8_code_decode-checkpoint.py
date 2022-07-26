85cf6f528226b093a09bec7dad188b01198e2805
feng-h <huangfeng0127@hotmail.com>
2022-07-26 16:19:53 +0000

    use function

--- .ipynb_checkpoints/Day8_code_decode-checkpoint.py
+++ .ipynb_checkpoints/Day8_code_decode-checkpoint.py
@@ -1,31 +1,63 @@
 # 字符转ASCII码 ord()
+# ASCII码转字符 chr()
+# 如果要使用函数，把声明的函数放在前面，因为python是一步一步执行的。
+def code():
+    code_letter = input("please input the word you want to code:\n")
+    code_number = int(input("input shift number:\n"))
+
+    a = list(code_letter)
+    b = list()
+    for i in a:
+        t = chr(ord(i) + code_number)
+        b.append(t)
+    print("your coded word is: \n")
+    print(''.join(b))
+    
+
+def decode():
+    decode_letter = input("please input the word you want to decode:\n")
+    decode_number = int(input("input shift number:\n"))
+
+    a = list(decode_letter)
+    b = list()
+    for i in a:
+        t = chr(ord(i) - decode_number)
+        b.append(t)
+    print("your decoded word is: \n")
+    print(''.join(b))
+
+
 print("Welcome to Code/Decode System!")
+
+
 
 while True:
     action_command = input("Do you want to Code or Decode:\n")
     if action_command == 'c' or action_command == 'C' or action_command == 'code':
-        code_letter = input("please input the word you want to code:\n")
-        code_number = int(input("input shift number:\n"))
+#         code_letter = input("please input the word you want to code:\n")
+#         code_number = int(input("input shift number:\n"))
 
-        a = list(code_letter)
-        b = list()
-        for i in a:
-            t = chr(ord(i) + code_number)
-            b.append(t)
-        print("your coded word is: \n")
-        print(''.join(b))
+#         a = list(code_letter)
+#         b = list()
+#         for i in a:
+#             t = chr(ord(i) + code_number)
+#             b.append(t)
+#         print("your coded word is: \n")
+#         print(''.join(b))
+        code()
         break
     elif action_command == 'd' or action_command == 'D' or action_command == 'decode':
-        decode_letter = input("please input the word you want to decode:\n")
-        decode_number = int(input("input shift number:\n"))
+#         decode_letter = input("please input the word you want to decode:\n")
+#         decode_number = int(input("input shift number:\n"))
 
-        a = list(decode_letter)
-        b = list()
-        for i in a:
-            t = chr(ord(i) - decode_number)
-            b.append(t)
-        print("your decoded word is: \n")
-        print(''.join(b))
+#         a = list(decode_letter)
+#         b = list()
+#         for i in a:
+#             t = chr(ord(i) - decode_number)
+#             b.append(t)
+#         print("your decoded word is: \n")
+#         print(''.join(b))
+        decode()
         break
     else:
         print("please input c or C or code for code.")
@@ -34,27 +66,3 @@ while True:
 
 
 
-# def code():
-#     code_letter = input("please input the word you want to code:\n")
-#     code_number = int(input("input shift number:\n"))
-
-#     a = list(code_letter)
-#     b = list()
-#     for i in a:
-#         t = chr(ord(i) + code_number)
-#         b.append(t)
-#     print("your coded word is: \n")
-#     print(''.join(b))
-    
-
-# def decode():
-#     decode_letter = input("please input the word you want to decode:\n")
-#     decode_number = int(input("input shift number:\n"))
-
-#     a = list(decode_letter)
-#     b = list()
-#     for i in a:
-#         t = chr(ord(i) - decode_number)
-#         b.append(t)
-#     print("your decoded word is: \n")
-#     print(''.join(b))
